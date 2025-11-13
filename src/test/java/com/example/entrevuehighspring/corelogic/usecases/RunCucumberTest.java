623a14764d92bdf456153d9e3cd46cc8a00b6951
Christopher Prevoe <programming@prevoe.com>
2025-11-13 11:57:37 -0500

    update POM so cucumber tests properly report

--- src/test/java/com/example/entrevuehighspring/corelogic/usecases/RunCucumberTest.java
+++ src/test/java/com/example/entrevuehighspring/corelogic/usecases/RunCucumberTest.java
@@ -1,17 +1,21 @@
 package com.example.entrevuehighspring.corelogic.usecases;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
-
 @Suite
 @IncludeEngines("cucumber")
-@SelectPackages("com.example")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example")
+@SelectPackages("com.example.entrevuehighspring.corelogic.usecases.bookrental")
+@SelectClasspathResource("com/example/entrevuehighspring/corelogic/usecases/bookrental")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.example.entrevuehighspring.corelogic.usecases.bookrental")
+@ConfigurationParameter(
+    key = io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME,
+    value = "pretty, html:target/cucumber-report.html"
+)
 public class RunCucumberTest {
 }

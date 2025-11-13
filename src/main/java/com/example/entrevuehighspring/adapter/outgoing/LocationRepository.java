3e397f0f14e2096cd3b301be0aa5fb2e2f1192e5
Christopher Prevoe <programming@prevoe.com>
2025-11-13 12:13:00 -0500

    remove LocationRepository adapter as adapters are out of scope

--- src/main/java/com/example/entrevuehighspring/adapter/outgoing/LocationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.entrevuehighspring.adapter.outgoing;
-
-import com.example.entrevuehighspring.domain.Location;
-import com.example.entrevuehighspring.domain.LocationId;
-
-public interface LocationRepository {
-    Location getLocationById(LocationId locationId);
-}
